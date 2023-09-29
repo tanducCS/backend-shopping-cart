92d62667c4b28ed557b4cf3926017471867db24a
tanducCS <99661719+tanducCS@users.noreply.github.com>
2023-09-29 11:37:52 +0700

    update CORS

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,8 +1,6 @@
-# config/initializers/cors.rb
-Rails.application.config.middleware.insert_before 0, Rack::Cors do
-    allow do
-      origins 'your-app-domain.com' # Thay 'your-app-domain.com' bằng tên miền thực của ứng dụng web của bạn
-      resource '*', headers: :any, methods: [:get, :post, :put, :patch, :delete, :options]
-    end
+config.middleware.insert_before 0, Rack::Cors do
+  allow do
+    origins '*'  
+    resource '*', headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head]
   end
-  
+end
